7e0663f14ab90e1d7cfd46fdef1c0e6ee1c97c01
meetshah1995 <meetshah1995@gmail.com>
2016-05-20 20:06:13 +0900

    Ignore build files

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to RISC-V&#8217;s documentation!"],titleterms:{document:0,indic:0,risc:0,tabl:0,welcom:0}})
