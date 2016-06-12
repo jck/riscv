85cd9da7200693ab52bd132856062f46a6941af0
meetshah1995 <meetshah1995@iitb.ac.in>
2016-06-12 21:57:22 +0900

    Refactor tests to remove nosetests and unittests

--- riscv/hdl_decoder.py
+++ riscv/hdl_decoder.py
@@ -1,2 +1,14 @@
 import decoder
 import instruction_table
+from myhdl import always_comb, Signal, Simulation, delay
+
+def hdl_decoder(instruction):
+    """
+    HDL decoder module to decode instructions from
+    RISC-V ISA.
+    """
+
+    @always_comb
+    def decoder_output():
+    
+
